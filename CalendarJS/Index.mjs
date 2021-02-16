4edb28db06732d070ffb34fdedd75865b2795757
Andrew <a.smirthwaite@gmail.com>
2021-02-16 17:28:18 +0000

    Fixed bug where logout logged out anyone

--- CalendarJS/Index.mjs
+++ CalendarJS/Index.mjs
@@ -1,7 +1,7 @@
 import WebSocket from './ws/index.js';
 import FileHander from './FileHandler.js';
 import User from './User.js';
-import {Guid} from 'js-guid';
+import { Guid } from 'js-guid';
 
 const wss = new WebSocket.Server({
     port: 8080,
@@ -25,13 +25,13 @@ wss.on('connection', (ws) => {
     ws.on('message', (data) => {
         let guid = data.replace(/([0-9a-z]+,,,)/gi, "");
         let location = data.indexOf(guid);
-        if(location != 0){
-        data = data.substring(0,location-3);
+        if (location != 0) {
+            data = data.substring(0, location - 3);
         }
         if (Users.length > 0) {
 
             let user = Users.find(element => element.UserClient === guid);
-            
+
             if (typeof user !== "undefined" && user.IsUserDM()) {
 
                 if (data === 'NextDay') {
@@ -57,23 +57,29 @@ wss.on('connection', (ws) => {
             if (userAndCode !== ".") {
                 let userName = userAndCode.replace(/(\.[a-z0-9]+)/gi, '');
                 let userCode = userAndCode.replace(/([a-z0-9]+\.)/gi, '');
-                var user = fileHander.ReadUserFromFile(userName, userCode);
 
-                if (user !== -1) {
-                    user.SetUserClient(Guid.newGuid().toString());
-                    Users.push(user);
-                    ws.send("Login Successful.." + user.UserRole+","+user.UserClient);
-                } else {
-                    ws.send("Login Unsuccessful.");
+                if (userName !== "" && userCode !== "") {
+                    var user = fileHander.ReadUserFromFile(userName, userCode);
+
+                    if (user !== -1) {
+                        user.SetUserClient(Guid.newGuid().toString());
+                        Users.push(user);
+                        ws.send("Login Successful.." + user.UserRole + "," + user.UserClient);
+                    } else {
+                        ws.send("Login Unsuccessful.");
+                    }
                 }
             }
         } else if (data.startsWith("LogOut")) {
             let user = Users.find(element => element.UserClient === guid);
-            let index = Users.indexOf(user);
-            let status = Users.splice(index, 1);
-
-            if (status.length !== 0) {
+            let status = -1;
+            if (typeof user !== "undefined") {
+                let index = Users.indexOf(user);
+                status = Users.splice(index, 1);
+            }
+            if (status.length !== 0 && status != -1) {
                 ws.send("Logout Successful.");
+                console.log(status);
             } else {
                 ws.send("Logout Unsuccessful.");
             }
